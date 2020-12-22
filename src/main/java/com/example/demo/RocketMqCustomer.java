62a98119944386781cd1e723af0ef02ffbc7f8af
miangong <mian.g@burgeon.cn>
2020-12-22 16:26:55 +0800

    更新

--- src/main/java/com/example/demo/RocketMqCustomer.java
+++ src/main/java/com/example/demo/RocketMqCustomer.java
@@ -22,9 +22,24 @@ public class RocketMqCustomer {
 
     public static void main(String[] args) throws MQClientException {
         //负载均衡模式
-        getMessageFzjh();
+//        getMessageFzjh();
         //广播模式
-        
+        getMessageGb();
+    }
+
+    private static void getMessageGb() throws MQClientException {
+        DefaultMQPushConsumer consumer = new DefaultMQPushConsumer("customer");
+        consumer.setNamesrvAddr("123.56.248.199:9876");
+        consumer.setMessageModel(MessageModel.BROADCASTING);
+        consumer.subscribe("Topic", "*");
+        consumer.registerMessageListener(new MessageListenerConcurrently() {
+            @Override
+            public ConsumeConcurrentlyStatus consumeMessage(List<MessageExt> list, ConsumeConcurrentlyContext consumeConcurrentlyContext) {
+                System.out.println("消费消息:" + list);
+                return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
+            }
+        });
+        consumer.start();
     }
 
     private static void getMessageFzjh() throws MQClientException {
@@ -36,7 +51,7 @@ public class RocketMqCustomer {
             @Override
             public ConsumeConcurrentlyStatus consumeMessage(List<MessageExt> msgs, ConsumeConcurrentlyContext consumeConcurrentlyContext) {
                 System.out.printf("%s Receive New Messages: %s %n", Thread.currentThread().getName(), msgs);
-                for (MessageExt mess:msgs){
+                for (MessageExt mess : msgs) {
                     System.out.println(mess.getBody());
                 }
                 return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
